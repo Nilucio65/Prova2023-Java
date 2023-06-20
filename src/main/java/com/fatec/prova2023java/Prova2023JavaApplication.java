e036fe97d44e735fc5c4bc38df6fc9dc803eaf0d
Nicolas Lucio <nilucio65@gmail.com>
2023-06-20 08:18:02 -0300

    cria projeto java

---
+++ src/main/java/com/fatec/prova2023java/Prova2023JavaApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.prova2023java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova2023JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova2023JavaApplication.class, args);
+	}
+
+}
