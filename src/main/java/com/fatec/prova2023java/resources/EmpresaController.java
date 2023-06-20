472ffc5ef906eb6b58bf4f2950347b5e58a04c35
Nicolas Lucio <nilucio65@gmail.com>
2023-06-20 09:38:52 -0300

    Adiciona listagem de empresas

--- src/main/java/com/fatec/prova2023java/resources/EmpresaController.java
+++ src/main/java/com/fatec/prova2023java/resources/EmpresaController.java
@@ -1,10 +1,12 @@
 package com.fatec.prova2023java.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +35,11 @@ public class EmpresaController {
        .toUri();
         
         return ResponseEntity.created(location).body(newEmpresa);
+    }
+
+    @GetMapping()
+    public List<Empresa> getEmpresa(){
+       return empresaService.getEmpresa();
     }
     
 }
